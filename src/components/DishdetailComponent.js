413a1ffb9fa3cd5f4055790b58740b27ae127c85
Wang Yu <yuwang8904@gmail.com>
2020-03-04 19:25:13 +0800

    assignment1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import {Card, CardImg, CardImgOverlay, CardBody, CardText,CardTitle} from 'reactstrap';
+
+class Dishdetail extends Component{
+    constructor(props){
+        super(props);
+        
+        console.log('Dishdetail Component constructor is invoked.')
+    }
+    renderDish(dish){
+        if(dish !=null){
+          return(
+              <Card>
+                  <CardImg width="100%" src={dish.image} alt={dish.name} />
+                  <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                  </CardBody>
+              </Card>
+          );
+        }else{
+          return(
+            <div></div>
+          );
+        }
+    };
+
+    renderComments(comments) {
+        if (comments != null) {
+            const comts = comments.map((comment) => {
+                return (
+                    <div>
+                        <br></br>
+                        {comment.comment}
+                        <br></br>
+                        <br></br>
+                        --{comment.author}, {comment.date}
+                        <br></br>
+                    </div>
+                )
+            });
+            return (
+                <div>
+                    <h4>Comments</h4>
+                    {comts}
+                </div>
+            );
+        } else {
+            return (
+                <div></div>
+            );
+        }
+    };
+
+    render(){
+        const dish = this.props.selectedDish;
+        const comments = dish ? dish.comments : null;
+        return(
+            <div className="row">
+                <div className="column col-12 col-md-5 m-1">
+                    {this.renderDish(dish)}
+                </div>
+                <div className="column col-12 col-md-5 m-1">                   
+                    {this.renderComments(comments)}
+                </div>
+            </div>
+        );
+    }; 
+}
+
+export default Dishdetail;
