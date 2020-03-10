fd4def4c68f8c53504d2d4adb1dc03e00b5113fa
Wang Yu <yuwang8904@gmail.com>
2020-03-10 18:59:28 +0800

    SPAPart2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 function RenderDish({ dish }) {
     if (dish != null) {
         return (
-            <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <div className="column col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
         );
     } else {
         return (
@@ -21,19 +24,20 @@ function RenderDish({ dish }) {
 
 function RenderComments({ comments }) {
     if (comments != null) {
-        const comts = comments.map((comment) => {
-            return (
-                <ul className="list-unstyled">
-                    <li>{comment.comment}</li>
-                    <li> --{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
-                    </li>
-                </ul>
-            )
-        });
         return (
-            <div>
+            <div className="column col-12 col-md-5 m-1">
                 <h4>Comments</h4>
-                {comts}
+                <ul className="list-unstyled">
+                    {comments.map((comment) => {
+                        return (
+                            <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p> --{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                                </p>
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
         );
     } else {
@@ -44,16 +48,22 @@ function RenderComments({ comments }) {
 };
 
 const DishDetail = (props) => {
-
     return (
         <div className="container">
             <div className="row">
-                <div className="column col-12 col-md-5 m-1">
-                    <RenderDish dish={props.dish} />
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
                 </div>
-                <div className="column col-12 col-md-5 m-1">
-                    <RenderComments comments={props.dish ? props.dish.comments : null} />
-                </div>
+            </div>
+            <div className="row">
+                <RenderDish dish={props.dish} />
+                <RenderComments comments={props.comments} />
             </div>
         </div>
     );
